ca2a1a7000113eebeaf816423c3e0cbee908c9cc
Walid Bouhdif <wbouhdif@hotmail.com>
2023-03-05 20:57:03 +0100

    removed redundant files, imported toastr module and added correct components to the module file

--- IPRWC-frontend/src/app/app-routing.module.ts
+++ IPRWC-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {LoginpageComponent} from "./loginpage/loginpage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'login', component: LoginpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IPRWC-frontend/src/app/app.module.ts
+++ IPRWC-frontend/src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
